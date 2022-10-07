82e0d494f16a6571b5862bce05ebbb921311569c
sahar-allouch <saharallouch@gmail.com>
2022-10-07 13:31:45 +0200

    add_script

---
+++ get_results_quantif.m
@@ -0,0 +1,17 @@
+% average connectvitiy matrices over epochs for each subject
+% results are saved in resutls/avg_cmats
+get_avg_cmats()
+
+% get group-level consistency
+% results are saved in results/grp_consistency
+get_grp_consistency()
+
+% get between-subejcts similarity
+% results are saved in results results/subs_similarity
+get_btw_subj_sim()
+
+% get within_subjects similarity
+% resutls are saved in results/epochs_similarity
+get_within_subj_sim()
+
+
